3900c99bca10035d423df4f8e134d1de6f3e52eb
John French <jdaviesf7@gmail.com>
2015-04-17 16:36:19 -0400

    Merged lexer and parser tests into a single file (because they are in the same module)

--- tests/test_lexparse.py
+++ tests/test_lexparse.py
@@ -1,16 +1,76 @@
 import unittest
 
 
-def _name(rule, symbolic=False):
-    from lexparse import LXScriptParser
-
-    if symbolic and (rule == -1):
-        return 'EOF'
-    else:
-        return (LXScriptParser.symbolicNames if symbolic else LXScriptParser.ruleNames)[rule]
-
-
 class LexerTest(unittest.TestCase):
+    def test_number(self):
+        self._lex_expect("42", [('NUMBER', '42')])
+
+    def test_name(self):
+        self._lex_expect("foo", [('NAME', 'foo')])
+
+    def test_symbols_and_keywords(self):
+        self._lex_expect("!${}[] out full @=", [
+            'BANG',
+            'DOLLARS',
+            'LBRACE',
+            'RBRACE',
+            'LBRACKET',
+            'RBRACKET',
+            'OUT',
+            'FULL',
+            'AT',
+            'EQUALS'
+        ])
+
+    def test_comment(self):
+        self._lex_expect("#blah blah 42 = something\n# this is another comment", [])
+
+    def test_mix(self):
+        self._lex_expect("led12 = {foo bar} #cool stuff\n!1 = led12 @ out", [
+            ('NAME', "led"),
+            ('NUMBER', "12"),
+            'EQUALS',
+            'LBRACE',
+            ('NAME', "foo"),
+            ('NAME', "bar"),
+            'RBRACE',
+            'BANG',
+            ('NUMBER', "1"),
+            'EQUALS',
+            ('NAME', "led"),
+            ('NUMBER', "12"),
+            'AT',
+            'OUT'
+        ])
+
+    def _lex_expect(self, code, expected_tokens):
+        """
+        Run some code through the lexer and check the tokens it returns.
+        :param code: The string of code to lex.
+        :param expected_tokens: A list where the values are either token type constants or tuples of token type
+            constants and token texts, with one item for each token expected in the lexer output from lexing the code.
+        :return: Does not return a value, but asserts that the output of the lexer matches the expected output.
+        """
+        from antlr4.InputStream import InputStream
+        from lexparse import LXScriptLexer
+        import itertools
+
+        lexer = LXScriptLexer(InputStream(code))
+        actual_tokens = [
+            (self._name(actual.type), actual.text) if type(expected) is tuple else self._name(actual.type)
+            for expected, actual in itertools.zip_longest(expected_tokens, lexer.getAllTokens())
+        ]
+
+        self.assertEqual(actual_tokens, expected_tokens)
+
+    def _name(self, rule):
+        """Get the name of a lexer rule from its index"""
+        from lexparse import LXScriptLexer
+
+        return LXScriptLexer.symbolicNames[rule]
+
+
+class ParserTest(unittest.TestCase):
     def test_system_literal_and_declaration(self):
         self._parse_expect(
             "1 = @@1",
@@ -133,13 +193,22 @@ class LexerTest(unittest.TestCase):
         def type_tree(parse_tree):
             """Converts a parse tree to the format used to specify expected parse trees, as described above"""
             if isinstance(parse_tree, TerminalNode):
-                return _name(parse_tree.getSymbol().type, True)
+                return self._name(parse_tree.getSymbol().type, True)
             else:
                 return (
-                    _name(parse_tree.getRuleIndex()),
+                    self._name(parse_tree.getRuleIndex()),
                     [type_tree(child) for child in parse_tree.getChildren()],
                 )
 
         actual_tree = type_tree(parser.lxscript())
 
         self.assertEquals(actual_tree, expected_tree)
+
+    def _name(self, rule, symbolic=False):
+        """Get the name of a parser rule (or of a terminal, if symbolic is True) from its rule index"""
+        from lexparse import LXScriptParser
+
+        if symbolic and (rule == -1):
+            return 'EOF'
+        else:
+            return (LXScriptParser.symbolicNames if symbolic else LXScriptParser.ruleNames)[rule]
