703c2a509b07658e7cdc6cc30c028876a7d7b865
John French <jdaviesf7@gmail.com>
2015-04-28 23:34:49 -0400

    Fixed model test to have addresses

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,19 +7,19 @@ from model.system import *
 class SystemTest(TestCase):
     def test_output_system(self):
         output = Mock()
-        s = OutputSystem(output)
+        s = OutputSystem(1, output)
         s.set_level(0.5)
-        output.set_level.assert_called_once_with(0.5)
+        output.set_level.assert_called_once_with(1, 0.5)
 
     def test_compound_system(self):
         outputs = [Mock(), Mock(), Mock()]
         cs = CompoundSystem([
-            OutputSystem(outputs[0]),
+            OutputSystem(1, outputs[0]),
             CompoundSystem([
-                OutputSystem(outputs[1]),
-                OutputSystem(outputs[2])
+                OutputSystem(1, outputs[1]),
+                OutputSystem(1, outputs[2])
             ])
         ])
         cs.set_level(0.25)
         for output in outputs:
-            output.set_level.assert_called_once_with(0.25)
+            output.set_level.assert_called_once_with(1, 0.25)
