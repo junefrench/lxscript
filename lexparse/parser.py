f534899268f725aed4b14b8b5fa127723a1aaae9
John French <jdaviesf7@gmail.com>
2015-04-30 04:02:36 -0400

    Added visitor to parser module

--- lexparse/parser.py
+++ lexparse/parser.py
@@ -1,4 +1,6 @@
-from ._gen import LXScriptParser
+import re
+
+from ._gen import LXScriptParser, LXScriptVisitor
 from . import error
 from antlr4 import CommonTokenStream
 
@@ -10,3 +12,14 @@ class Parser(LXScriptParser.LXScriptParser):
         super(Parser, self).__init__(input=CommonTokenStream(input))
         # Should call self.removeAllErrorListeners() but that's not available in current ANTLR python runtime
         self._listeners = [error._ExceptionErrorListener(error.ParseError)]
+
+
+class Visitor(LXScriptVisitor.LXScriptVisitor):
+    """A visitor for LXScript parse trees"""
+    _visit_regex = re.compile(r'^visit_([a-z])(.*)$')
+
+    def __getattr__(self, name):
+        # Patch the visitor so that visit_foo works as an alias for visitFoo
+        match = self._visit_regex.match(name)
+        new_name = 'visit{0}{1}'.format(match.group(1).upper(), match.group(2))
+        return getattr(self, new_name)
